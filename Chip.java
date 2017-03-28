f54013e8516cd862806198927e9e2330c40f8a58
JasonLalonde <js595390@dal.ca>
2017-03-28 19:57:48 -0300

    hi
    hi

--- Chip.java
+++ Chip.java
@@ -1,6 +1,46 @@
-
+// Chip class
 public class Chip {
-
-
-
+	// attributes 
+	private String name;
+	private int value;
+	
+	// constructor
+	public Chip(String n, int c){
+		if(n.toLowerCase().equals("hundred") && c==100){
+			name=n;
+			value=c;
+		}
+		else if(n.toLowerCase().equals("fifty") && c==50){
+			name=n;
+			value=c;
+		}
+		else if(n.toLowerCase().equals("twentyfive") && c==25){
+			name=n;
+			value=c;
+		}
+		else if(n.toLowerCase().equals("ten") && c==10){
+			name=n;
+			value=c;
+		}
+	}
+	
+	// get methods
+	public String getName(){
+		return name;
+	}
+	public int getValue(){
+		return value;
+	}
+	// set methods
+	public void setName(String n){
+		name=n;
+	}
+	public void setValue(int c){
+		value=c;
+	}
+	
+	// toString method
+	public String toString(){
+		return "Name: " +name+ "\nValue: " +value;
+	}
 }
