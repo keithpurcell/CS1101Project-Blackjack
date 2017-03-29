8e60a1760c5d494d525b0d905eb1af2ea8a66748
SeanDevine <sean_devine@hotmail.ca>
2017-03-28 22:19:45 -0300

    mm

--- Blackjack/src/BlackjackDemo.java
+++ Blackjack/src/BlackjackDemo.java
@@ -4,27 +4,5 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BlackjackDemo {
-	/*public static ArrayList<Chip> createStack(int buyIn){
-		ArrayList<Chip> stack = new ArrayList<Chip>();
-		Chip Hundred = new Chip("hundred", 100);
-		Chip fifty = new Chip();
-		while (buyIn != 0){
-			if (buyIn - 100 >= 0){
-				stack.add
-			}
-		}
-	}
-	*/
-	public static void main (String[] Args){
-		
-		String name;
-		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter your name: ");
-		name = kb.nextLine();
-		Player player = new Player(name, 500);
-		
-		while (!player.isBroke()){
-			
-		}
-	}
+	//mrkndbfj
 }
