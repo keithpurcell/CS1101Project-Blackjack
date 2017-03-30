7921d6712869f464f8ddffc1d3d1a7e5c3e8a215
SeanDevine <sean_devine@hotmail.ca>
2017-03-30 20:58:28 -0300

    Player
    Player class with ints instead of chips

--- Player.java
+++ Player.java
@@ -3,85 +3,54 @@ package blackjack;
 import java.util.ArrayList;
 
 public class Player {
-  private String name;
-  private ArrayList <Chip> stack = new ArrayList <Chip>();
-  private Hand hand;
-  int total;
-  
-  public Player(String n, Arraylist <Chip> s, Hand h){
-    name = n;
+	private String name;
+	private int stack;
+	private Hand hand;
+	int total;
+
+	public Player(String n, int s) {
+		name = n;
+		stack = s;
+	}
+
+	// get and set methods
+	public String getName() {
+		return name;
+	}
+
+	public int getStack() {
+		return stack;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void setStack(int s){
     stack = s;
-    hand = h;
-    setTotal();
   }
-  
-  //get and set methods
-  public String getName(){
-    return name;
-  }
-  public ArrayList<Chip> getStack(){
-    return stack;
-  }
-  public Hand getHand(){
-    return hand;
-  }
-  public int getTotal(){
-    return total;
-  }
-  public void setName(String n){
-    name = n;
-  }
-  public void setStack(ArrayList <Chip> s){
-    stack = s
-  }
-  public void setHand(Hand h){
-    hand = h;
-  }
-  public void setTotal(){
-    for (int i=0;i<stack.size();i++){
-      total += chip.getValue(stack.get(i));
-  }
-  
-  //other methods
-  public boolean isBroke(){
-    if (stack.isEmpty())
-      return true
+
+	public void setHand(Hand h) {
+		hand = h;
+	}
+
+	// other methods
+	public boolean isBroke(){
+    if (stack == 0)
+    	return true;
     else
-      return false
-  }
-  public void addChip(Chip c){
-    stack.add(c);
-    setTotal();
-  }
-  public void subtractChip(Chip c){
-    if (!stack.contains(c))
-      return false
-    else {
-      stack.remove(c);
-      setTotal();
-    }
-  }
-  
-  //Hit Method
-  public void hit(Card c) {
-    hand.add(c);
-  }
-  
-  //canSplit
-  public boolean canSplit() {
-    boolean result = false;
-    if(hand.getHand().get(0).getID().equals(hand.getHand().get(1).getID()) && hand.getHand().get(0).getSuit().equals(hand.getHand().get(1).getSuit()))
-      result = true;
-    return result;
-  }
-  
-  //Split Method
-  public Card split() {
-    return (hand.getHand.remove(1));
-  }
-  
-  //toString
-  public String toString(){
-      return name+": $"+getTotal();
-  }
+    	return false;
+	}
+
+	// toString
+	public String toString() {
+	}
 }
